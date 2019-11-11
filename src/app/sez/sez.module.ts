2367738397f5a5fe20fe4a65ab6f216ff81eb84b
mdrokz <mohammadmunshi@gmail.com>
2019-11-11 20:12:40 +0530

    added smart-table and removed tsc

--- src/app/sez/sez.module.ts
+++ src/app/sez/sez.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
-import { NbMenuModule } from '@nebular/theme';
-
+import { NbMenuModule, NbCardModule } from '@nebular/theme';
 import { ThemeModule } from '../@theme/theme.module';
 import { SezComponent } from './sez.component';
 import { UsersComponent } from './users/users.component';
 import { SezRoutingModule } from './sez-routing.module';
 import { MiscellaneousModule } from '../pages/miscellaneous/miscellaneous.module';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 @NgModule({
     imports: [
         SezRoutingModule,
         ThemeModule,
         NbMenuModule,
-        MiscellaneousModule
+        MiscellaneousModule,
+        Ng2SmartTableModule,
+        NbCardModule
     ],
     declarations: [
         SezComponent,
--- src/app/sez/users/users.component.ts
+++ src/app/sez/users/users.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LocalDataSource } from 'ng2-smart-table';
+
+import { SmartTableData } from '../../@core/data/smart-table';
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
@@ -7,7 +11,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+      confirmDelete: true,
+    },
+    columns: {
+      FullName: {
+        title: 'Name',
+        type: 'string',
+      },
+      ITS: {
+        title: 'ITS#',
+        type: 'string',
+      },
+      Mobile: {
+        title: 'Mobile',
+        type: 'number',
+      },
+      Age: {
+        title: 'Age',
+        type: 'number',
+      },
+      Title: {
+        title: 'Title',
+        type: 'string',
+      },
+      SectorIncharge: {
+        title: 'Sector Incharge',
+        type: 'string'
+      },
+      SpecialSkills: {
+        title: 'Special Skills',
+        type: 'string'
+      }
+    },
+  };
+
+  source: LocalDataSource = new LocalDataSource();
+
+  constructor(private service: SmartTableData) {
+    const data = this.service.getData();
+    this.source.load(data);
+  }
+
+  onDeleteConfirm(event): void {
+    if (window.confirm('Are you sure you want to delete?')) {
+      event.confirm.resolve();
+    } else {
+      event.confirm.reject();
+    }
+  }
 
   ngOnInit() {
   }
