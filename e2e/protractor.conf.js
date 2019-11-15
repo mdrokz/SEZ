0381d727da0c9667286406635779938579454de8
Murtaza <murtazamunshi@live.com>
2019-11-15 15:57:21 +0530

    Changed port of app

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://localhost:7700/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
