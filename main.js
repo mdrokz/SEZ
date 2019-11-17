4d9409e67ee668a0474b55b56ceb8da63b4453b8
Murtaza <murtazamunshi@live.com>
2019-11-17 15:38:27 +0530

    Connected mongo db to cloud

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 // Modules to control application life and create native browser window
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 var electron_1 = require("electron");
 var mongoose = require('mongoose');
@@ -14,13 +14,25 @@ var route = require('./data-access/route');
 var win, serve;
 var args = process.argv.slice(1);
 serve = args.some(function (val) {
-  return val === '--serve';
+    return val === '--serve';
 });
 // connect to mongodb
-mongoose.connect("mongodb://localhost:27017/test", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+// var mongoUrl = "mongodb://localhost:27017/test";
+var mongoUrl = "mongodb+srv://sez:sez%40123@sez-zgsak.mongodb.net/sez";
+
+mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
+
+mongoose.connection.on('open', (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Mongo is connected...');
+    }
+});
+
 var used = process.memoryUsage().heapUsed / 1024 / 1024;
 console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
 // ipc communication
@@ -114,41 +126,41 @@ electron_1.ipcMain.on("api", function (event, arg) {
 // used = process.memoryUsage().heapUsed / 1024 / 1024;
 // console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
 function createWindow() {
-  // Create the browser window.
-  win = new electron_1.BrowserWindow({
-    width: 1366,
-    height: 768,
-    webPreferences: {
-      nodeIntegration: true,
-      backgroundThrottling: false
-    }
-  });
-  // and load the index.html of the app.
-  // win.loadURL(
-  //   url.format({
-  //     pathname: path.join(__dirname, `/dist/electron-poc/index.html`),
-  //     protocol: "file:",
-  //     slashes: true
-  //   })
-  // );
-  // and load the index.html of the app.
-  if (serve) {
-    require('electron-reload')(__dirname, {
-      electron: require(__dirname + "/node_modules/electron")
+    // Create the browser window.
+    win = new electron_1.BrowserWindow({
+        width: 1366,
+        height: 768,
+        webPreferences: {
+            nodeIntegration: true,
+            backgroundThrottling: false
+        }
     });
-    win.loadURL('http://localhost:7700');
-  } else {
-    win.loadFile('./dist/electron-poc/index.html');
-  }
-  // Open the DevTools.
-  win.webContents.openDevTools();
-  // Emitted when the window is closed.
-  win.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
+    // and load the index.html of the app.
+    // win.loadURL(
+    //   url.format({
+    //     pathname: path.join(__dirname, `/dist/electron-poc/index.html`),
+    //     protocol: "file:",
+    //     slashes: true
+    //   })
+    // );
+    // and load the index.html of the app.
+    if (serve) {
+        require('electron-reload')(__dirname, {
+            electron: require(__dirname + "/node_modules/electron")
+        });
+        win.loadURL('http://localhost:7700');
+    } else {
+        win.loadFile('./dist/electron-poc/index.html');
+    }
+    // Open the DevTools.
+    win.webContents.openDevTools();
+    // Emitted when the window is closed.
+    win.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null;
+    });
 }
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -156,19 +168,20 @@ function createWindow() {
 electron_1.app.on('ready', createWindow);
 // Quit when all windows are closed.
 electron_1.app.on('window-all-closed', function () {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    electron_1.app.quit();
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        electron_1.app.quit();
+    }
 });
 electron_1.app.on('activate', function () {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow();
+    }
 });
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 //# sourceMappingURL=main.js.map
+console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
